52d7df2b348a9591840d267c8ae716187943c21a
yuhuananshen <112676978+yuhuananshen@users.noreply.github.com>
2022-10-07 17:03:16 +0800

    Update CalcTest.java

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -8,5 +8,11 @@ public class CalcTest {
     void testAddition() {
         Calc c = new Calc();
         assertEquals(4, c.add(2,2));
+
+    }
+    @Test
+    void testSubtract() {
+    	Calc c = new Calc();
+    	assertEquals(2, c.subtract(4,2));
     }
 }
